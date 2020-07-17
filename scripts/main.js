6d7571ad878b7466091767f7025cb8cc44ff5b73
Sabathick <erickick13@gmail.com>
2020-07-16 22:57:02 -0300

    Aula teste JQuery+Bootstrap+Ajax

--- scripts/main.js
+++ scripts/main.js
@@ -6,11 +6,25 @@ function consultaCep(){
         type: "GET",
         success: function(response){
             console.log(response);
-            document.getElementById("logradouro").innerHTML = response.logradouro;
-            document.getElementById("bairro").innerHTML = response.bairro;
-            document.getElementById("localidade").innerHTML = response.localidade;
-            document.getElementById("uf").innerHTML = response.uf;
+            $("#logradouro").html(response.logradouro);
+            $("#uf").html(response.uf);
+            $("#localidade").html(response.localidade);
+            $("#bairro").html(response.bairro);
+            $("#titulo_cep").html("CEP  " + response.cep);
+            $(".cep").show();
+            $(".barra-progresso").show();
+            $(".barra-progresso").hide();
+            //document.getElementById("logradouro").innerHTML = response.logradouro;
+            //document.getElementById("bairro").innerHTML = response.bairro;
+            //document.getElementById("localidade").innerHTML = response.localidade;
+            //document.getElementById("uf").innerHTML = response.uf;
         }
         
     })
 }
+
+$(function(){
+    $(".cep").hide(); 
+    $(".barra-progresso").hide();   
+});
+
